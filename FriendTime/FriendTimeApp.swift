01ada3a10ef147818808e1f40fa36870dc15d0d9
akosek913 <162849817+akosek913@users.noreply.github.com>
2025-10-18 16:23:25 -0700

    Initial Commit

---
+++ FriendTime/FriendTimeApp.swift
@@ -0,0 +1,17 @@
+//
+//  FriendTimeApp.swift
+//  FriendTime
+//
+//  Created by Alyson Kosek on 10/18/25.
+//
+
+import SwiftUI
+
+@main
+struct FriendTimeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FriendTimeTests/FriendTimeTests.swift
@@ -0,0 +1,17 @@
+//
+//  FriendTimeTests.swift
+//  FriendTimeTests
+//
+//  Created by Alyson Kosek on 10/18/25.
+//
+
+import Testing
+@testable import FriendTime
+
+struct FriendTimeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
